fab053f0d2b9ce05fdd2f6a46d2258db2e3ab5c3
Chris Hall <80642332+gitorion@users.noreply.github.com>
2025-09-05 10:01:09 +0100

    refactor: Impreovements to asset info

--- mystart.py
+++ mystart.py
@@ -425,7 +425,7 @@ class MyStart:
         self.vars["messages"].insert(23, msg21)
         self.vars["messages"].insert(24, line_border)
         welcome = (
-            f"{Fore.WHITE}User: %s Host: %s " - "%s"
+            f"{Fore.WHITE}User: %s Host: %s\t%s"
         ) % (self.vars.get("user"), self.vars.get("host"), self.vars.get("host_task"))
 
         self.vars["messages"].insert(1, welcome)
